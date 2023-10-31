d5caf3d54940cc80075c927dff5d8d51b0225930
PiyushYadav777 <136405153+PiyushYadav777@users.noreply.github.com>
2023-10-31 11:38:41 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,47 @@
+package Assignment;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int currentVal = romanCharToInt(s.charAt(i));
+            int nextVal = (i + 1 < s.length()) ? romanCharToInt(s.charAt(i + 1)) : 0;
+
+            if (currentVal < nextVal) {
+                result -= currentVal;
+            } else {
+                result += currentVal;
+            }
+        }
+
+        return result;
+    }
+
+    public static int romanCharToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        String romanNumeral = "IX";
+        int integerEquivalent = romanToInt(romanNumeral);
+        System.out.println("The integer equivalent of " + romanNumeral + " is " + integerEquivalent);
+    }
+}
